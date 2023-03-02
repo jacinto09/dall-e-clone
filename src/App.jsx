b7c6074de5799265ef4c45693cb06131df1c35f5
Jacinto Martinez <jacintomartinez0909@gmail.com>
2023-03-02 01:46:49 +0100

    first commit :D

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 
 import './App.css'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import logo from './assets/logo.svg'
+import {Home, CreatePost} from './pages'
 
 function App() {
 
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world
-    </h1>
-  )
-}
+ <BrowserRouter>
+ 
+ <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b">
+    <Link to='/'>
+      <img src={logo} alt="logo" className='object-container w-28 '/>
+    </Link>
+    <Link to='/create-post' className='font-medium text-white bg-black px-4 py-2 rounded-md'>
+      Create
+    </Link>
+ </header>
+ <main className="sm:p-8 px-4 py-8 w-full bg-slate-400 min-h-[calc(100vh-73px)]">
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/createpost" element={<CreatePost/>}/>
+  </Routes>
+ </main>
+ 
+ 
+ </BrowserRouter>
+)}
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React,{useState, useEffect} from 'react'
+import {Loader, Card, FormField} from '../components'
+const Home = () => {
+
+const [loading, SetLoading]= useState(false)
+
+
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <h1>
+        The community showcase
+      </h1>
+    </section>
+  )
+}
+
+export default Home
